be05d55588fbc5a1f7dc7ee977c0f7c56d361ab4
Nuria <soriano.nuria@gmail.com>
2021-09-09 12:00:01 +0200

    add lesson 7-1: critical css end

--- 71-critical-css/webpack.prod.js
+++ 71-critical-css/webpack.prod.js
@@ -1,13 +1,38 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
-
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  plugins: [new GenerateSW(), new MiniCssExtractPlugin()],
+  plugins: [
+    new GenerateSW(),
+    new MiniCssExtractPlugin(),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, "dist"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      minify: true,
+      extract: true,
+      dimensions: [
+        {
+          width: 375,
+          height: 565,
+        },
+        {
+          width: 1100,
+          height: 700,
+        },
+      ],
+      penthouse: {
+        blockJSRequests: false,
+      },
+    }),
+  ],
   module: {
     rules: [
       {
